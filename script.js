f3443b16fc5b002f5a9392a715035b7b0f8bc657
Ines Zenkri <ines@zenkri.com>
2024-01-03 19:47:35 +0100

    fixed the palcement of the name in center

--- script.js
+++ script.js
@@ -29,15 +29,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const startX = 135; 
     const endX = 740; 
     const rangeWidth = endX - startX;
-    const fontSize = 58; //initial 
+    let fontSize = 58; //initial 
     const font = await pdfDoc.embedFont(PDFLib.StandardFonts.Helvetica);
-    const textWidth = font.widthOfTextAtSize(fullName, fontSize);
-
+    let textWidth = font.widthOfTextAtSize(fullName, fontSize);
+    console.log(textWidth);
+    console.log(rangeWidth);
+    console.log(fontSize);
+    if(textWidth > rangeWidth) {
+      console.log("doesn't fit in");
+      const scale = rangeWidth / textWidth;
+      fontSize *= scale ;
+      textWidth = font.widthOfTextAtSize(fullName, fontSize);
+      console.log(textWidth);
+      console.log(fontSize);
+    }
     const textStartX = startX + (rangeWidth - textWidth) / 2;
     firstPage.drawText(fullName, {
       x: textStartX,
       y: 270,
-      size: 58,
+      size: fontSize,
       color: rgb(0.71, 0.514, 0.839),
     });
 
@@ -60,13 +70,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const lastName = document.getElementById("lastNameInput").value.trim();
     if (firstName !== "" && lastName !== "") {
       generatePDF(firstName, lastName);
-      alert("Certificate Downloaded");
       document.getElementById("firstNameInput").value = "";
       document.getElementById("lastNameInput").value = "";
       document.getElementById("emailInput").value = "";
+      alert("Certificate Downloaded");
     } else {
       console.log("He was too shy to give his first/last name");
       alert("Please d'ont be shy and supply us with your infos 🥺");
     }
   });
 });
+
